2b39a21a143a2cd86fc231e39c74a8830f5c0235
Oleg Domanov <odomanov@yandex.ru>
2024-08-13 23:31:12 +0700

    Added      extensions/org-roam-prop-search.el

---
+++ extensions/org-roam-prop-search.el
@@ -0,0 +1,128 @@
+;;; org-roam-prop-search.el --- Org-roam property search interface -*- coding: utf-8; lexical-binding: t; -*-
+
+;; Copyright © 2020-2022 Jethro Kuan <jethrokuan95@gmail.com>
+
+;; Author: Jethro Kuan <jethrokuan95@gmail.com>
+;; URL: https://github.com/org-roam/org-roam
+;; Keywords: org-mode, roam, convenience
+;; Version: 2.2.2
+;; Package-Requires: ((emacs "26.1") (org "9.4") (org-roam "2.1"))
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Commentary:
+;;
+;; This package provides the necessary changes required to make org-export work out-of-the-box.
+;;
+;; To enable it, run:
+;;
+;;    (require 'org-roam-prop-search)
+;;
+;;
+;;; Code:
+
+(use-package helm)
+(require 'emacsql)
+(require 'emacsql-sqlite-builtin)
+(require 'ol)
+(require 'dash)
+
+(defcustom org-roam-props-to-filter
+  '("CATEGORY" "ITEM" "FILE" "ALLTAGS" "ID" "PRIORITY" "TITLE"
+    "REL" "TAGS" "TODO")
+  "Properties to filter."
+  :group 'org-roam)
+
+(defface org-roam-props--property-face
+    '((t (:foreground "blue"
+          :weight bold
+          )))
+  "Face for 'properties' in search."
+  :group 'org-roam-faces)
+
+(defun org-roam-props--completions ()
+  "Make list [prop,value,file] from list [properties,file]."
+  (let* ((props+files (org-roam-db-query [:select [properties,file] :from nodes]))
+         (to-filter org-roam-props-to-filter)
+         (raw-lst (seq-mapcat (lambda (props+file)
+                                (seq-mapcat (lambda (prop)
+                                              (mapcar (lambda (x)
+                                                        (list (car prop) x (cadr props+file)))
+                                                      (split-string-and-unquote (cdr prop))))
+                                            (car props+file)))
+                              props+files)))
+    (seq-remove (lambda (p)
+                  (member (car p) to-filter))
+                raw-lst)))
+
+(defun odm/sort-uniq (n lst)
+  "Make LST unique and sorted by the Nth element.
+Comparison functions are `string<' and `'string='."
+  (-sort (lambda (x y) (string< (elt x n) (elt y n)))
+         (let ((-compare-fn (lambda (x y)
+                              (string= (elt x n) (elt y n)))))
+           (-uniq lst))))
+
+(defun org-roam-props--mk-prop-candidates (filtered)
+  ""
+  (-map (lambda (x)
+          (let ((str (concat (propertize (format ":%s: " (elt x 0))
+                                         'face 'org-roam-props--property-face)
+                             (elt x 1))))
+            (cons str x)))
+        filtered))
+
+(defun org-roam-props--mk-val-candidates (v filtered)
+  ""
+  (let ((flt (-filter (lambda (x) (string= (cadr v) (cadr x)))
+                      filtered)))
+    (-map (lambda (x)
+            (let ((str (concat (propertize
+                                (format ":%s: " (string-join `(,(elt x 0) ,(elt x 1)) ":"))
+                                'face 'org-roam-props--property-face)
+                               (file-name-base (elt x 2)))))
+              (cons str x)))
+          flt)))
+
+(defun org-roam-search-property ()
+  "Search properties interactively."
+  (interactive)
+  (let* ((props (org-roam-props--completions))
+         (prop (completing-read "Property: " (odm/sort-uniq 0 props)))
+         (filtered (-filter (lambda (x) (equal (car x) prop)) props))
+         )
+    (pcase prop
+      ("title" (let* ((vals (-map #'cdr filtered))
+                      (val (completing-read (concat prop ": ") vals)))
+                 (find-file (concat org-roam-directory "/"
+                                    (cadr (-find (lambda (v)
+                                                      (string= val (car v)))
+                                                    vals))))))
+      (_ (helm :sources (helm-build-sync-source "Properties"
+                          :candidates (odm/sort-uniq 2 (org-roam-props--mk-prop-candidates filtered))
+                          :action (lambda (v)
+                                    (helm :sources
+                                          (helm-build-sync-source "Values"
+                                            :candidates
+                                            (odm/sort-uniq 3 (org-roam-props--mk-val-candidates v filtered))
+                                            :action (lambda (f) (find-file (elt f 2)))))))))
+      )))
+
+
+(provide 'org-roam-prop-search)
+;;; org-roam-prop-search.el ends here
